977480a86509afaff092845c16c18846c195ee14
rowan04 <rowan.moss@stfc.ac.uk>
2026-01-07 13:32:47 +0000

    Rest framework tutorial part 1 - serialization

--- tutorial/testsite/urls.py
+++ tutorial/testsite/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("quickstart/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("snippets/", include("snippets.urls"))
 ]
